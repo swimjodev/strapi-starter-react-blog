55dc5eb40309fe586c09bf0ddfdec9d348e73ed4
swimjodev <84591504+swimjodev@users.noreply.github.com>
2021-05-22 22:29:47 +0300

    Update index.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,23 +1,71 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import emailjs from "emailjs-com";
 
-import { Switch, Route } from "react-router-dom";
+//import "./ContactUs.css";
+import "./App.css";
 
-import Nav from "../../components/Nav";
-import Articles from "../Articles";
-import Article from "../Article";
-import Category from "../Category";
+export class App extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Switch>
-        <Route path="/" component={Articles} exact />
-        <Route path="/article/:id" component={Article} exact />
-        <Route path="/category/:id" component={Category} exact />
-      </Switch>
-    </div>
-  );
+  getdata() {
+    axios("http://localhost:1337/articles").then((i) => {
+      console.log(i["data"][0]["image"]["url"]);
+      this.setState({ img: i["data"][0]["image"]["url"] });
+    });
+  }
+
+  sendEmail = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    emailjs
+      .send(
+        "service_ol5dmcb",
+        "template_5pw4t8j",
+        this.state,
+        "user_oGk6T9qq030b5pAbLtycy"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  changeHandler = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleParagraphChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value.split("\\n").join("\n"),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form className="contact-form" noValidate onSubmit={this.sendEmail}>
+          <label>Name</label>
+          <input type="text" name="name" onChange={this.changeHandler} />
+          <label>Email</label>
+          <input type="email" name="email" onChange={this.changeHandler} />
+          <label>Message</label>
+          <textarea name="message" onChange={this.handleParagraphChange} />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
